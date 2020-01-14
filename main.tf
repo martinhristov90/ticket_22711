961b5e0fdffb5184866c519c56a561a2cfa6d81d
Martin Hristov <mhristov@hashicorp.com>
2020-01-14 08:46:39 +0200

    uncomment aws_instance.web

--- main.tf
+++ main.tf
@@ -32,11 +32,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-#resource "aws_instance" "web" {
-#  ami           = data.aws_ami.ubuntu.id
-#  instance_type = "t2.micro"
-#
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
