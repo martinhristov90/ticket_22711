3945d3aa9c0a71221ed1f5388833a1b1f87266cb
Martin Hristov <mhristov@hashicorp.com>
2020-01-22 19:05:16 +0200

    adding specific verison aws

--- main.tf
+++ main.tf
@@ -13,37 +13,26 @@ provider "aws" {
   version = "~> 2.45.0"
   region = "us-east-1"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-
-//data "aws_ami" "ubuntu" {
-//  most_recent = true
-//
-//  filter {
-//    name   = "name"
-//    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-//  }
-//
-//  filter {
-//    name   = "virtualization-type"
-//    values = ["hvm"]
-//  }
-//
-//  owners = ["099720109477"] # Canonical
-//}
-//
-//resource "aws_instance" "web" {
-//  ami           = data.aws_ami.ubuntu.id
-//  instance_type = "t2.micro"
-//
-//  tags = {
-//    Name = "HelloWorld"
-//  }
-//}
-
-resource "null_resource" "name" {
-
-  provisioner "local-exec" {
-    command = "free -m"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
-  
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
